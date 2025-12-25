cfdab826f8a2c68110d42f66db7ebc07174ad2d3
Kosmos258 <maksimowkirill@gmail.com>
2025-12-25 19:23:39 +0300

    fix

--- src/agents/api_tasks/api_task1/front/src/pages/AllUsers.tsx
+++ src/agents/api_tasks/api_task1/front/src/pages/AllUsers.tsx
@@ -4,54 +4,49 @@ import { IUser } from '../shared/types/IUser';
 import './AllUsers.scss';
 
 const AllUsers: React.FC = () => {
-	const [users, setUsers] = useState<IUser[]>([]);
+  const [users, setUsers] = useState<IUser[]>([]);
 
-	useEffect(() => {
-		getUsers().then((data) => {
-			console.log('Получили данные:', data);
+  const loadUsers = () => {
+    getUsers().then((data) => {
+      if (Array.isArray(data)) {
+        setUsers(data);
+      } else {
+        setUsers([]);
+      }
+    });
+  };
 
-			if (Array.isArray(data)) {
-				setUsers(data);
-			} else {
-				setUsers([]);
-			}
-		});
-	}, []);
+  useEffect(() => {
+    loadUsers();
+  }, []);
 
-	return (
-		<div className="users-container">
-			<h1>Список пользователей</h1>
-			<button
-				onClick={() => {
-					getUsers().then((data) => {
-						if (Array.isArray(data)) {
-							setUsers(data);
-						}
-					});
-				}}
-			>
-				Обновить
-			</button>
+  return (
+    <div className="users-container">
+      <h1>Список пользователей</h1>
 
-			{users.length === 0 && <p>Пользователей нет</p>}
+      <button onClick={loadUsers}>
+        Обновить
+      </button>
 
-			<div className="users-list">
-				{users.map((user) => (
-					<div key={user.id} className="user-card">
-						<h3>{user.name}</h3>
-						<p>@{user.username}</p>
-						<p>Email: {user.email}</p>
-						<p>Телефон: {user.phone}</p>
-						<p>Сайт: {user.website}</p>
-						<p>
-							Адрес: {user.address.city}, {user.address.street}
-						</p>
-						<p>Компания: {user.company.name}</p>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+      {users.length === 0 && <p>Пользователей нет</p>}
+
+      <div className="users-list">
+        {users.map((user) => (
+          <div key={user.id} className="user-card">
+            <h3>{user.name}</h3>
+            <p>@{user.username}</p>
+            <p>Email: {user.email}</p>
+            <p>Телефон: {user.phone}</p>
+            <p>Сайт: {user.website}</p>
+            <p>
+              Адрес: {user.address.city}, {user.address.street}
+            </p>
+            <p>Компания: {user.company.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;

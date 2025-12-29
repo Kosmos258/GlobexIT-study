712707e79adc77a3c2a2c8ea1f3d29166424e91b
Kosmos258 <maksimowkirill@gmail.com>
2025-12-29 14:48:55 +0300

    fix3

--- src/agents/system_events/system_events.ts
+++ src/agents/system_events/system_events.ts
@@ -1,19 +1,5 @@
 declare const learningDoc: LearningDocumentTopElem;
 
-interface IError {
-	code: number;
-	message: string;
-}
-
-/**
- * Создает поток ошибки с объектом error
- * @param {string} source - источник ошибки
- * @param {IError} errorObject - объект ошибки
- */
-function HttpError(source: string, errorObject: IError) {
-	throw new Error(source + " -> " + errorObject.message);
-}
-
 /**
  * Выбирает все записи sql запроса
  * @param {string} query - sql-выражение
@@ -31,37 +17,31 @@ function selectOne<T>(query: string, defaultObj: any = undefined) {
 	return ArrayOptFirstElem<T>(tools.xquery(`sql: ${query}`), defaultObj);
 }
 
-try {
-	const personId = learningDoc.person_id.Value;
-	learningDoc.state_id.Value;
+const personId = learningDoc.person_id.Value;
+const finishedCourseState = learningDoc.state_id.Value;
 
-	const allLearnings = selectAll<{ state_id: XmlElem<number>; }>(`
-			SELECT 
-                course_id, 
-                course_name, 
-                state_id
-			FROM dbo.learnings
-			WHERE person_id = '${personId}';
-		`);
+const allLearnings = selectAll<{ state_id: number }>(`
+	SELECT 
+		course_id, 
+		course_name, 
+		state_id
+	FROM dbo.learnings
+	WHERE person_id = '${personId}';
+`);
 
-	allLearnings.filter(
-		(l) => OptInt(l.state_id.Value) !== 4,
-	).length;
+const failedAttempts = allLearnings.filter(
+	(l) => OptInt(l.state_id) !== 4,
+).length;
 
+if (failedAttempts < 3 && finishedCourseState === 4) {
+	const newCourse = selectOne<{ id: XmlElem<number> }>(`
+		SELECT 
+			id
+		FROM dbo.courses
+		WHERE status = 'publish'
+		ORDER BY RANDOM()
+		LIMIT 1;
+	`);
 
-	const newCourse = selectOne<{ id: XmlElem<number>; }>(`
-			SELECT 
-                id
-			FROM dbo.courses
-			WHERE status = 'publish'
-			ORDER BY RANDOM()
-			LIMIT 1;
-		`);
-
-	tools.activate_course_to_person(personId, newCourse.id.Value);
-} catch (error) {
-	throw HttpError("Handler", {
-		code: 500,
-		message: error.message,
-	});
+	tools.activate_course_to_person(personId, newCourse.id);
 }
